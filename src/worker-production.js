f94238ede3a01182e1c0f7fe2a2a4a37ff683799
evertondback <everton@dback.net>
2026-09-06 13:59:54 -0400

    Unify operations center and fix critical runtime flows

--- src/worker-production.js
+++ src/worker-production.js
@@ -1,9 +1,9 @@
 import app from './worker-v10.js';
 
-const PRODUCTION_VERSION='31.0.0';
+const PRODUCTION_VERSION='32.0.0';
 function patchHtml(html) {
   let out = html;
-  for (const v of ['7.0.1','7.0.2','8.0.0','8.0.1','8.1.0','9.0.0','9.0.1','9.0.2','9.1.0','9.2.0','9.3.0','9.4.0','9.5.0','9.6.0','9.7.0']) out = out.replaceAll(v,PRODUCTION_VERSION);
+  for (const v of ['7.0.1','7.0.2','8.0.0','8.0.1','8.1.0','9.0.0','9.0.1','9.0.2','9.1.0','9.2.0','9.3.0','9.4.0','9.5.0','9.6.0','9.7.0','31.0.0']) out = out.replaceAll(v,PRODUCTION_VERSION);
   out = out.replace("Start Today's Workout", "Start Today Workout");
   out = out.replace('<div id=videoDock class="videoDock normal">', '<div id=videoDock class="videoDock">');
   out = out.replace(
